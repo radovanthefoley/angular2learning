8cb795d60a50332d9d423e9be7cbe9c298217de1
Radovan Masaryk <rmasaryk@gmail.com>
2017-01-31 13:49:10 +0100

    (click) select hero, conditionally enabling details again

--- app/app.component.ts
+++ app/app.component.ts
@@ -24,16 +24,19 @@ const HEROES: Hero[] = [
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class='heroes'>
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes"
+        (click)="onSelectedHero(hero)">
         <span class='badge'>{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <!--<h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>-->
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name">
+      </div>
+    </div>
     `,
   styles: [`
   .heroes {
@@ -80,4 +83,9 @@ const HEROES: Hero[] = [
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelectedHero(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
