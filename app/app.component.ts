1ff4d3424649550aeb54abdca9ce1636f1bbacff
Radovan Masaryk <rmasaryk@gmail.com>
2017-02-02 11:46:38 +0100

    content moved out of the component files (html, css)
    Note: moduleId: module.id attribute added to each component in order to preserve relative paths (full paths to html/css need to be used otherwise)

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
-  `
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'Tour of Heroes';
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -3,17 +3,9 @@ import { Component, Input } from '@angular/core';
 import { Hero } from './hero';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-    </div>
-    `
+  templateUrl: './hero-detail.component.html'
 })
 export class HeroDetailComponent {
   @Input()
