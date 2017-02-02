75f6089df97b56e6b692f9ef2d34f78f135a6407
Radovan Masaryk <rmasaryk@gmail.com>
2017-02-02 10:59:56 +0100

    AppComponent refactored (will be used for routing exclusively), HeroesComponent created

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule }   from '@angular/forms'
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroDetailComponent],
+  declarations: [AppComponent, HeroDetailComponent, HeroesComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
