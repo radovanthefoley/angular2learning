e629e97482265410b8aac4e15a23c04c4eb3c60d
Radovan Masaryk <rmasaryk@gmail.com>
2017-02-16 20:30:28 +0100

    refactoring routing to its own AppRoutingModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule }  from '@angular/router';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
@@ -8,28 +7,13 @@ import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
