412409235e552f8cf87ad497201bc0abdcf3ab9f
Radovan Masaryk <rmasaryk@gmail.com>
2017-02-16 19:53:48 +0100

    adding parametrized url routing to hero-detail (removing @Input and master detail layout)
    Notes:
    	- Note how the switchMap operator maps the id in the observable route parameters to a new Observable, the result of the HeroService.getHero method.
    
    	- If the user re-navigates to this component while a getHero request is still inflight, switchMap cancels that old request before calling HeroService.getHero again.
    
    	- The hero id is a number. Route parameters are always strings. So we convert the route parameter value to a number with the JavaScript (+) operator.

--- app/app.module.ts
+++ app/app.module.ts
@@ -24,6 +24,10 @@ import { HeroService } from './hero.service';
       {
         path: 'dashboard',
         component: DashboardComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
       }
     ])
   ],
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,13 +1,25 @@
-import { Component, Input } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   moduleId: module.id,
   selector: 'my-hero-detail',
   templateUrl: './hero-detail.component.html'
 })
-export class HeroDetailComponent {
-  @Input()
+export class HeroDetailComponent implements OnInit {
+  constructor(private heroService: HeroService,
+              private route: ActivatedRoute) {
+  }
+
   hero: Hero;
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+      .subscribe(hero => this.hero = hero);
+  }
 }
