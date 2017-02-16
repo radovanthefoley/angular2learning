3f51598f4eb3b48a904ded74ffd4041bce196929
Radovan Masaryk <rmasaryk@gmail.com>
2017-02-16 17:50:07 +0100

    * TypeScript version upgrade, semicolons fix (node_modules/@types/jasmine/index.d.ts(39,52): error TS1005: '=' expected.)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule }  from '@angular/router';
-import { FormsModule }   from '@angular/forms'
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard.component';
