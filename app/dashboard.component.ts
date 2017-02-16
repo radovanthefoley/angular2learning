550a38dd437296c7cf91dfb3fe4518a477776628
Radovan Masaryk <rmasaryk@gmail.com>
2017-02-16 18:48:00 +0100

    dashboard data population

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
   moduleId: module.id,
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html'
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+  topHeroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.heroService.getHeroes().then(heroes => this.topHeroes = heroes.slice(0, 4));
+  }
+}
