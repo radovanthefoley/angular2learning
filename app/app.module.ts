d50f0788a06b9995a4259c95085d8e0aada431b6
Radovan Masaryk <rmasaryk@gmail.com>
2017-02-22 17:54:28 +0100

    adding mock InMemoryWebApiModule, effectively replacing the Http client's XHR backend service with an in-memory alternative.

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -15,6 +19,7 @@ import { AppRoutingModule } from "./app-routing.module";
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [
