a5c7accaaaf59fb655cfe63f17747c5a16feda6e
Radovan Masaryk <rmasaryk@gmail.com>
2017-02-22 13:54:13 +0100

    adding @angular/http for global use

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard.component';
@@ -13,6 +14,7 @@ import { AppRoutingModule } from "./app-routing.module";
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   declarations: [
